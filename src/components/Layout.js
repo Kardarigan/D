d54f121b077205ba16b2d27adf810e36ec751a48
D <113527210+Kardarigan@users.noreply.github.com>
2024-02-03 12:09:16 -0700

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,30 @@
 import Sidebar from './Sidebar'
 import '../style/App.scss';
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
-    return <><Sidebar /></>
+    return (
+        <div className='App'>
+            <Sidebar />
+            <div className='tags'>
+
+                <span className='tags-top'>
+                    <span className="tags-top-html">&lt;html&gt;</span>
+                    <br />
+                    <span className='tags-top-body'>&lt;body&gt;</span>
+                </span>
+
+                <Outlet />
+
+                <span className='tags-bottom'>
+                    <span className='tags-bottom-body'>&lt;/body&gt;</span>
+                    <br />
+                    <span className="tags-bottom-html">&lt;/html&gt;</span>
+                </span>
+
+            </div>
+        </div>
+    )
 }
 
 export default Layout
